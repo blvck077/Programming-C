74ba4ea64671b3206649cc33db59bd4c38e79ebb
blvck077 <75691852+blvck077@users.noreply.github.com>
2021-05-13 11:44:06 +0300

    Add files via upload

---
+++ C5.2.12/C5.2.12/C5.2.12.cpp
@@ -0,0 +1,137 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <locale.h>
+
+
+typedef struct Node { // узел дерева
+    char data;
+    Node* parent;
+    Node* left;
+    Node* right;
+}Node;
+
+char* enterString();
+Node* addNode(Node*, char);
+void insertNode(Node**, char);
+void createTreeFromString(Node** tree);
+void showTree(Node*, const char*, int);
+void deleteTree(Node*&);
+int leavesCount(Node*);
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Node* tree = NULL;
+    createTreeFromString(&tree);
+    showTree(tree, "Корень: ", 0);
+    printf("Количество листьев в дереве: %d", leavesCount(tree));
+    deleteTree(tree);
+}
+
+char* enterString() {
+    char* str = (char*)malloc(sizeof(char)); 
+    int i = 0; 
+    while ((str[i] = getchar()) != '\n') { 
+        str = (char*)realloc(str, (++i + 1) * sizeof(char)); 
+    }
+    
+    str[i] = '\0'; 
+    return str;
+}
+
+Node* addNode(Node* parent, char data) {
+    Node* tmp = NULL;
+    if (!(tmp = (Node*)malloc(sizeof(Node)))) {
+        return 0;
+    }
+    tmp->left = tmp->right = NULL;
+    tmp->data = data;
+    tmp->parent = parent;
+    return tmp;
+}
+
+
+void insertNode(Node** head, char data) {
+    Node* tmp = NULL;
+    if (*head == NULL) {
+        *head = addNode(NULL, data);
+        return;
+    }
+    tmp = *head;
+    while (tmp) {
+        if (data > tmp->data) {
+            if (tmp->right != NULL) {
+                tmp = tmp->right;
+                continue;
+            }
+            
+            else {
+                tmp->right = addNode(tmp, data);
+                return;
+            }
+        }
+        else if (data < tmp->data) {
+
+            if (tmp->left != NULL) {
+                tmp = tmp->left;
+                continue;
+            }
+            else {
+                tmp->left = addNode(tmp, data);
+                return;
+            }
+        }
+
+        else {
+            return;
+        }
+    }
+}
+
+
+void createTreeFromString(Node** head) {
+    printf("Введите строку, которую мы добавим в дерево: ");
+    rewind(stdin);
+    char* strToInsert = enterString();
+    int i = 0;
+    while (strToInsert[i] != '\0') {
+        insertNode(head, strToInsert[i]);
+        i++;
+    }
+    free(strToInsert);
+}
+
+void showTree(Node* parent, const char* location, int depthLevel) {
+    if (parent) {
+        printf("Глубина = %d, %s = %c\n", depthLevel, location, parent->data);
+            showTree(parent->left, "Левое: ", depthLevel + 1);
+        showTree(parent->right, "Правое: ", depthLevel + 1);
+    }
+}
+
+int leavesCount(Node* root) {
+    int result;
+    if ((root->left == NULL) && (root->right == NULL)) {
+        result = 1;
+    }
+    else {
+        result = 0;
+    }
+    if (root->left != NULL) {
+        result += leavesCount(root->left);
+    }
+    if (root->right != NULL) {
+        result += leavesCount(root->right);
+    }
+    return result;
+}
+
+void deleteTree(Node*& tree) {
+    if (tree->left) {
+        deleteTree(tree->left);
+    }
+    if (tree->right) {
+        deleteTree(tree->right);
+    }
+    free(tree);
+    tree = NULL;
+}
